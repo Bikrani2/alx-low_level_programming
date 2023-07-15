235f7abcc58294c2ee6f9fcf840511cd1dfc9027
Bikrani2 <bikrani.fatimazahrae@gmail.com>
2023-07-15 08:24:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 gcc -wall grammar. -o output 
+#include <stdio.h>
 int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return 0;
 }
 
