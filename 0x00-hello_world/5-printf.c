17ca92ce42db636c75e83f2a3725f53d37b7aa4f
Bikrani2 <bikrani.fatimazahrae@gmail.com>
2023-07-15 08:16:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-/**
- * main - A program that prints a line using the printf function
- * Return: 0 (Success)
- */
+gcc -wall grammar. -o output 
 int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return 0;
 }
-gcc -wall grammar.c -o output 
+
